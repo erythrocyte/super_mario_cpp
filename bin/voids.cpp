b7453c1fa90cf461f85dbc7a3870bd5f213b1242
erythrocyte <marseille.tatar@yahoo.com>
2022-10-30 01:10:42 +0300

    upload assets

--- bin/voids.cpp
+++ bin/voids.cpp
@@ -11,8 +11,7 @@ namespace g = sm::src::gui;
 namespace sm::bin {
 void run()
 {
-    std::string a = "Tests";
-    auto frame = std::make_shared<g::Frame>(200, 200, a);
+    auto frame = std::make_shared<g::Frame>(200, 200, "Tests");
 
     auto circle = std::make_shared<sf::CircleShape>(100.f);
     circle->setFillColor(sf::Color::Green);
