540223ee5a2667ca4ffb2e3dd20648fcc5bc421f
erythrocyte <marseille.tatar@yahoo.com>
2022-10-30 00:56:38 +0300

    compile done

--- bin/main.cpp
+++ bin/main.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <memory>
 
-// #include "lib/logging/logger.hpp"
+#include "lib/logging/logger.hpp"
 #include "voids.hpp"
 
 int main(int argc, char** argv)
 {
-    // std::string fn = "a.log";
-    // sm::src::lib::logging::init_log(fn);
+    std::string fn = "a.log";
+    sm::src::lib::logging::init_log(fn);
 
-    // sm::bin::run();
+    sm::bin::run();
 
     return 0;
 }
--- bin/voids.cpp
+++ bin/voids.cpp
@@ -2,23 +2,23 @@
 
 #include <string>
 
-// #include <SFML/Graphics.hpp>
+#include <SFML/Graphics.hpp>
 
-// #include "gui/frame.hpp"
+#include "gui/frame.hpp"
 
-// namespace g = sm::src::gui;
+namespace g = sm::src::gui;
 
 namespace sm::bin {
 void run()
 {
     std::string a = "Tests";
-    // auto frame = std::make_shared<g::Frame>(200, 200, a);
+    auto frame = std::make_shared<g::Frame>(200, 200, a);
 
-    // auto circle = std::make_shared<sf::CircleShape>(100.f);
-    // circle->setFillColor(sf::Color::Green);
+    auto circle = std::make_shared<sf::CircleShape>(100.f);
+    circle->setFillColor(sf::Color::Green);
 
-    // frame->add_drawable(circle);
+    frame->add_drawable(circle);
 
-    // frame->run();
+    frame->run();
 }
 }
