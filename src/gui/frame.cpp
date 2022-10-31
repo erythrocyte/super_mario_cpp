72fdb780f1091c7eb86198f1dd7b11bba288f4d6
erythrocyte <marseille.tatar@yahoo.com>
2022-10-31 14:10:10 +0300

    success build with enemies vector

--- src/gui/frame.cpp
+++ src/gui/frame.cpp
@@ -22,51 +22,19 @@ void Frame::set_tiles(const std::vector<std::shared_ptr<slm::Tile>>& tiles)
 void Frame::run()
 {
     while (m_window->isOpen()) {
+        update_time();
 
-        float time = m_clock->getElapsedTime().asMicroseconds();
-        m_clock->restart();
+        check_events();
 
-        time = time / 500; // здесь регулируем скорость игры
+        check_keyboard_press();
 
-        if (time > 20) // this time only for smooth move of mario;
-            time = 20;
+        update_object_time();
 
-        sf::Event event;
-        while (m_window->pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                m_window->close();
-        }
+        check_enemies_intersect();
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-            m_mario->vx = -0.1; // set velocity x
-        }
+        update_offsets();
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-            m_mario->vx = 0.1; // set velocity x
-        }
-
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-            if (m_mario->onGround) { // jump only if we on ground;
-                m_mario->vy = -0.27;
-                m_mario->onGround = false;
-            }
-        }
-
-        m_mario->update(time);
-
-        if (m_mario->rect->left > 200) {
-            this->update_offset_x(m_mario->rect->left - 200); // смещение
-        }
-
-        m_window->clear(*m_def_bg_color);
-        for (auto const& tile : m_tiles) {
-            if (tile == nullptr)
-                continue;
-            m_window->draw(*tile);
-        }
-
-        m_window->draw(*m_mario->sprite);
-        m_window->display();
+        update_window();
     }
 }
 
@@ -77,6 +45,116 @@ void Frame::update_offset_x(float value)
         tile->set_offset_x(value);
         tile->update_position();
     }
+}
+
+void Frame::draw_tiles()
+{
+    for (auto const& tile : m_tiles) {
+        if (tile == nullptr)
+            continue;
+        m_window->draw(*tile);
+    }
+}
+
+void Frame::draw_enemies()
+{
+    for (auto const& enemy : m_enemies) {
+        if (enemy == nullptr || !enemy->alife)
+            continue;
+        m_window->draw(*enemy->sprite);
+    }
+}
+
+void Frame::add_enemy(std::shared_ptr<slm::Enemy> enemy)
+{
+    m_enemies.push_back(enemy);
+}
+
+void Frame::update_enemies_time()
+{
+    for (auto const& enemy : m_enemies) {
+        if (enemy == nullptr)
+            continue;
+        enemy->update(m_time);
+    }
+}
+
+void Frame::update_time()
+{
+    float time = m_clock->getElapsedTime().asMicroseconds();
+    m_clock->restart();
+
+    time = time / 500; // здесь регулируем скорость игры
+
+    if (time > 20) // this time only for smooth move of mario;
+        time = 20;
+}
+
+void Frame::check_enemies_intersect()
+{
+    for (auto const& enemy : m_enemies) {
+        if (m_mario->rect->intersects(*enemy->rect)) {
+            if (enemy->alife) {
+                if (m_mario->vy > 0) {
+                    enemy->vx = 0;
+                    m_mario->vy = -0.2;
+                    enemy->alife = false;
+                } else {
+                    m_mario->sprite->setColor(sf::Color::Red);
+                    break;
+                }
+            }
+        }
+    }
+}
+
+void Frame::check_keyboard_press()
+{
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+        m_mario->vx = -0.1; // set velocity x
+    }
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+        m_mario->vx = 0.1; // set velocity x
+    }
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+        if (m_mario->onGround) { // jump only if we on ground;
+            m_mario->vy = -0.27;
+            m_mario->onGround = false;
+        }
+    }
+}
+
+void Frame::check_events()
+{
+    sf::Event event;
+    while (m_window->pollEvent(event)) {
+        if (event.type == sf::Event::Closed)
+            m_window->close();
+    }
+}
+
+void Frame::update_object_time()
+{
+    m_mario->update(m_time);
+    update_enemies_time();
+}
+
+void Frame::update_offsets()
+{
+    if (m_mario->rect->left > 200) {
+        this->update_offset_x(m_mario->rect->left - 200); // смещение
+    }
+}
+
+void Frame::update_window()
+{
+    m_window->clear(*m_def_bg_color);
+    draw_tiles();
+    draw_enemies();
+    m_window->draw(*m_mario->sprite);
+    m_window->display();
 }
 
 }
--- src/gui/frame.hpp
+++ src/gui/frame.hpp
@@ -9,6 +9,7 @@
 
 #include <SFML/Graphics.hpp>
 
+#include "lib/models/enemy.hpp"
 #include "lib/models/player.hpp"
 #include "lib/models/tile.hpp"
 
@@ -22,6 +23,7 @@ public:
     void run();
     void set_mario(std::shared_ptr<slm::Player> mario);
     void set_tiles(const std::vector<std::shared_ptr<slm::Tile>>& tiles);
+    void add_enemy(std::shared_ptr<slm::Enemy> enemy);
 
 private:
     std::shared_ptr<sf::RenderWindow> m_window;
@@ -29,8 +31,21 @@ private:
     std::vector<std::shared_ptr<slm::Tile>> m_tiles;
     std::shared_ptr<sf::Clock> m_clock;
     std::shared_ptr<sf::Color> m_def_bg_color;
+    std::vector<std::shared_ptr<slm::Enemy>> m_enemies;
+    float m_time;
 
     void update_offset_x(float value);
+    void draw_tiles();
+    void draw_enemies();
+    void update_enemies_time();
+    void update_time();
+    void check_enemies_intersect();
+    void check_keyboard_press();
+    void check_events();
+    void update_object_time();
+    void update_offsets();
+
+    void update_window();
 };
 }
 
